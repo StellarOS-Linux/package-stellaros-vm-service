4ba83370d5e2beb2f73c808eba1ecdd4b1c984cd
LiAlH4 <LiAlH4_Tr@Outlook.com>
2023-12-08 18:20:16 +0800

    init fix 3

--- pkg/stellaros-vm-service/usr/lib/stellaros/save-vm.fish
+++
@@ -1,9 +0,0 @@
-#!/bin/env fish
-
-set list (VBoxManage list runningvms)
-for vm in $list
-    set a (string split "{" $vm)
-    set b (string split "}" $a[2])
-    set id $b[1]
-    VBoxManage controlvm $id savestate
-end
